5e42589587fe7ed634a6f3b8158b91a35f959e6e
uriyahr <uriyahruiz1@gmail.com>
2020-07-12 04:10:34 -0700

    Create server for testing

---
+++ server/src/companyRoutes.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = 'mongodb+srv://uriyahann:@cluster0.xvpyf.mongodb.net/pwyw-web-db?retryWrites=true&w=majority';
+const client = new MongoClient(uri, {
+  useNewUrlParser: true
+},{
+  useUnifiedTopology: true
+});
+const companySchema = new mongoose.Schema({
+  "_id": Number,
+  "companyName": String,
+  "companyURL": String,
+  "industry": String,
+  "description": String
+});
+
+const Company = mongoose.model('Company', companySchema, 'pwyw-companies');
+
+router.get('/', async (req,res) => {
+  try {
+    let companies = await Company.find();
+    console.log(companies);
+    return res.send(companies);
+  } catch (error) {
+    return res.sendStatus(500);
+  }
+});
+
+module.exports = {
+  model: Company,
+  routes: router
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,41 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    company: null,
+    companies: []
   },
   mutations: {
+    // company mutations
+    setCompany(state,company){
+      state.company = company;
+    },
+    setCompanies(state,companies){
+      state.companies = companies;
+    }
   },
   actions: {
+    // company actions
+    async getCompany(context, payload) {
+      try {
+        let response = await axios.get("/api/company/" + payload._id);
+        context.commit("setCompany", response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async getCompanies(context) {
+      try {
+        let response = await axios.get("/api/company/");
+        context.commit('setCompanies', response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
   },
   modules: {
   }
